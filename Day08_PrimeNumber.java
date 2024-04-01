c71a1f789e0cbde52a2acf87d45e55c23b95852a
lileshwar0217 <lileshpatil0217@gmail.com>
2024-04-01 14:14:15 +0530

    Day08_PrimeNumber

---
+++ Day08_PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Day08_PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+       
+    }
+
+    // Method to check if a number is prime
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
